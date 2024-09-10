d1dc82115ce832ecb89f4b2d7fa226b8111e72e5
artalon <felix.pinty@gmail.com>
2024-09-10 09:14:08 +0200

    test(fizzBuzz): ajout du test should return fizz when given 3 refacto(fizzbuzz): refacto pour répondre au nouveau test

--- specs/main.spec.ts
+++ specs/main.spec.ts
@@ -1,11 +1,19 @@
 import { fizzBuzz } from "../src/main";
 
 describe("fizz buzz", () => {
-    it("should return 1 when given 1", () => {
-        expect(fizzBuzz(1)).toEqual(1);
+
+    describe("when it should return number", ()=> {
+        it("should return 1 when given 1", () => {
+            expect(fizzBuzz(1)).toEqual(1);
+        });
+    
+        it("should return 2 when given 2", () => {
+            expect(fizzBuzz(2)).toEqual(2);
+        });
+    });
+
+    it("should return fizz when given 3", () => {
+        expect(fizzBuzz(3)).toEqual("fizz");
     })
 
-    it("should return 2 when given 2", () => {
-        expect(fizzBuzz(2)).toEqual(2);
-    })
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
-export function fizzBuzz(valueToCheck: number): number {
+export function fizzBuzz(valueToCheck: number): number | string {
+    if (valueToCheck === 3) {
+        return "fizz"
+    }
     return valueToCheck;
 }
